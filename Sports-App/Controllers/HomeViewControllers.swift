0afce8c10a2ce37e05ab896b946da0ec59860bfb
Mohamed Ali <mohammedali12477@gmail.com>
2021-03-27 13:01:23 -0700

    First Commit

--- Sports-App/Controllers/HomeViewControllers.swift
+++ Sports-App/Controllers/HomeViewControllers.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewControllers: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Sports-AppTests/Sports_AppTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sports_AppTests.swift
+//  Sports-AppTests
+//
+//  Created by Mohamed Ali on 3/27/21.
+//
+
+import XCTest
+
+class Sports_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
